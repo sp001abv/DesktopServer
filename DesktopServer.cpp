54fb091894f89bd7b6c54315fee905c91a0fd681
sp001abv <48313355+sp001abv@users.noreply.github.com>
2020-04-10 19:03:27 +0300

    Add files via upload

--- DesktopServer.cpp
+++ DesktopServer.cpp
@@ -9,6 +9,7 @@
 #include <ws2tcpip.h>
 #pragma comment (lib, "Ws2_32.lib")
 
+
 #include "DesktopServer.h"
 
 #define MAX_LOADSTRING 100
@@ -106,7 +107,7 @@ int m_screenHeight = 0;
 int m_screenSize = m_screenWidth * m_screenHeight;
 UINT m_bufferSize = m_screenWidth * m_screenHeight * 4;
 LONGLONG m_startTime = 0;
-LONGLONG m_sentFrames = 0;
+DWORD m_sentFrames = 0;
 LONGLONG m_updatedPixels = 0;
 LONGLONG m_sentBytes = 0;
 LONGLONG m_receivedBytes = 0;
@@ -125,7 +126,7 @@ void InitLog()
 }
 
 
-void InitMem()
+void InitScreenMem()
 {
 	RECT rc;
 	GetWindowRect(GetDesktopWindow(), &rc);
@@ -152,18 +153,13 @@ void InitMem()
 	m_screenBuffer = GlobalAlloc(GHND, m_screenSize * 4);
 }
 
-SOCKET m_clientSocket = INVALID_SOCKET;
-SOCKET m_listenSocket = INVALID_SOCKET;
-#define WM_SOCKET WM_USER + 1
+SOCKET m_clientScreenSocket = INVALID_SOCKET;
+SOCKET m_listenScreenSocket = INVALID_SOCKET;
 
 
-HRESULT InitSocket(HWND hWnd)
+HRESULT InitSocketSystem()
 {
 	HRESULT hr = S_OK;
-	addrinfo* resultAddress = NULL;
-	addrinfo hints;
-	char portStr[8] = { '\0' };
-
 	// initialize the socket system
 	WSADATA wsaData;
 	int result = WSAStartup(0x202, &wsaData);
@@ -172,6 +168,15 @@ HRESULT InitSocket(HWND hWnd)
 		hr = HRESULT_FROM_WIN32(WSAGetLastError());
 		return hr;
 	}
+	return hr;
+}
+
+HRESULT InitSocket(SOCKET& listenSocket, UINT port)
+{
+	HRESULT hr = S_OK;
+	addrinfo* resultAddress = NULL;
+	addrinfo hints;
+	char portStr[8] = { '\0' };
 
 	do
 	{
@@ -181,10 +186,10 @@ HRESULT InitSocket(HWND hWnd)
 		hints.ai_protocol = IPPROTO_IP;
 		hints.ai_flags = AI_PASSIVE;
 
-		sprintf_s(portStr, 8, "%d", 18080);
+		sprintf_s(portStr, 8, "%d", port);
 
 		// Resolve the server address and port
-		result = getaddrinfo(NULL, portStr, &hints, &resultAddress);
+		int result = getaddrinfo(NULL, portStr, &hints, &resultAddress);
 		if (result != 0)
 		{
 			hr = HRESULT_FROM_WIN32(WSAGetLastError());
@@ -192,9 +197,9 @@ HRESULT InitSocket(HWND hWnd)
 		}
 
 		// Create a SOCKET for connecting to server
-		//m_listenSocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
-		m_listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-		if (m_listenSocket == INVALID_SOCKET)
+		//listenSocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
+		listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		if (listenSocket == INVALID_SOCKET)
 		{
 			hr = HRESULT_FROM_WIN32(WSAGetLastError());
 			break;
@@ -202,7 +207,7 @@ HRESULT InitSocket(HWND hWnd)
 
 		// configure the socket
 		//int nZero = 0;
-		//result = setsockopt(m_listenSocket, SOL_SOCKET, SO_SNDBUF, (char *)&nZero, sizeof(nZero));
+		//result = setsockopt(listenSocket, SOL_SOCKET, SO_SNDBUF, (char *)&nZero, sizeof(nZero));
 		if (result == SOCKET_ERROR)
 		{
 			hr = HRESULT_FROM_WIN32(WSAGetLastError());
@@ -210,7 +215,7 @@ HRESULT InitSocket(HWND hWnd)
 		}
 
 		// Setup the TCP listening socket
-		result = bind(m_listenSocket, resultAddress->ai_addr,
+		result = bind(listenSocket, resultAddress->ai_addr,
 			(int)(resultAddress->ai_addrlen));
 		if (result == SOCKET_ERROR)
 		{
@@ -222,7 +227,7 @@ HRESULT InitSocket(HWND hWnd)
 		resultAddress = NULL;
 
 		// start listening on the input socket
-		result = listen(m_listenSocket, SOMAXCONN);
+		result = listen(listenSocket, SOMAXCONN);
 		if (result == SOCKET_ERROR)
 		{
 			hr = HRESULT_FROM_WIN32(WSAGetLastError());
@@ -243,30 +248,30 @@ HRESULT InitSocket(HWND hWnd)
 }
 
 
-HRESULT SendData(const void* data, DWORD dataLength)
+HRESULT SendData(SOCKET& socket, const void* data, DWORD dataLength)
 {
 	HRESULT hr = S_OK;
 	int sendResult = 0;
 
-	sendResult = send(m_clientSocket, (const char*)data, dataLength, 0);
+	sendResult = send(socket, (const char*)data, dataLength, 0);
 	if (sendResult == SOCKET_ERROR)
 	{
 		hr = HRESULT_FROM_WIN32(WSAGetLastError());
-		shutdown(m_clientSocket, SD_SEND);
-		m_clientSocket = INVALID_SOCKET;
+		shutdown(socket, SD_SEND);
+		socket = INVALID_SOCKET;
 		return hr;
 	}
 	return hr;
 }
 
-bool ReadBytes(char * buf, int size)
+bool ReadBytes(SOCKET& socket, char * buf, int size)
 {
-	int result = recv(m_clientSocket, buf, size, MSG_WAITALL);
+	int result = recv(socket, buf, size, MSG_WAITALL);
 	if (result == SOCKET_ERROR)
 	{
 		HRESULT hr = HRESULT_FROM_WIN32(WSAGetLastError());
-		shutdown(m_clientSocket, SD_SEND);
-		m_clientSocket = INVALID_SOCKET;
+		shutdown(socket, SD_SEND);
+		socket = INVALID_SOCKET;
 		return false;
 	}
 	m_receivedBytes += result;
@@ -274,9 +279,9 @@ bool ReadBytes(char * buf, int size)
 }
 
 template<typename T>
-bool read(T& t)
+bool read(SOCKET& socket, T& t)
 {
-	return ReadBytes((char*)&t, sizeof(t));
+	return ReadBytes(socket, (char*)&t, sizeof(t));
 }
 
 #define MOUSE_LEFTDOWN	1	
@@ -321,11 +326,11 @@ bool map(WORD& c1, WORD& c2, CODEMAP m[])
 	return false;
 }
 
-void Read()
+void ReadDesktopControl()
 {
 
 	BYTE type;
-	if (!read(type))
+	if (!read(m_clientScreenSocket, type))
 		return;
 	
 	if (type >= MOUSE_LEFTDOWN && type <= MOUSE_MOVE) {
@@ -353,7 +358,7 @@ void Read()
 			input.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
 			break;
 		case MOUSE_WHEEL:
-			if (read(data)) {
+			if (read(m_clientScreenSocket, data)) {
 				input.mi.mouseData = data * 120;
 				input.mi.dwFlags = MOUSEEVENTF_WHEEL;
 			}
@@ -362,7 +367,7 @@ void Read()
 		{
 			DWORD dx = 0;
 			DWORD dy = 0;
-			if (read(dx) && read(dy)) {
+			if (read(m_clientScreenSocket, dx) && read(m_clientScreenSocket, dy)) {
 				input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
 				input.mi.dx = dx * 65536 / m_screenWidth;
 				input.mi.dy = dy * 65536 / m_screenHeight;
@@ -377,7 +382,7 @@ void Read()
 	}
 	else if (type >= KEY_DOWN && type <= KEY_UP)  {
 		WORD code;
-		if (read(code))
+		if (read(m_clientScreenSocket, code))
 		{
 			INPUT input;
 			input.type = INPUT_KEYBOARD;
@@ -402,41 +407,43 @@ void Read()
 	}
 }
 
-HRESULT Accept()
+HRESULT Accept(SOCKET& clientSocket,const SOCKET& listenSocket)
 {
 	HRESULT hr = S_OK;
 
-	if (m_clientSocket != INVALID_SOCKET)
+	if (clientSocket != INVALID_SOCKET)
 		return S_FALSE;
 
 	// Accept a client socket connection
-	m_clientSocket = accept(m_listenSocket, NULL, NULL);
-	if (m_clientSocket == INVALID_SOCKET)
+	clientSocket = accept(listenSocket, NULL, NULL);
+	if (clientSocket == INVALID_SOCKET)
 	{
 		hr = HRESULT_FROM_WIN32(WSAGetLastError());
 		return hr;
 	}
+	return hr;
+}
 
-	InitMem();
+void InitScreenCapture() 
+{
+	InitScreenMem();
 
 	int dim = m_screenWidth << 16 | m_screenHeight;
-	SendData(&dim, sizeof(dim));
+	SendData(m_clientScreenSocket, &dim, sizeof(dim));
 
 	UINT* buffer = (UINT*)GlobalLock(m_screenBuffer);
 	memset(buffer, 0, m_screenSize * 4);
 	GlobalUnlock(m_screenBuffer);
 
 	m_startTime = GetTickCount64();
-
-	return hr;
 }
 
-bool IsDisconnected()
+inline bool IsSocketDisconnected(const SOCKET& socket)
 {
-	return m_clientSocket == INVALID_SOCKET;
+	return socket == INVALID_SOCKET;
 }
 
-void Capture()
+void CaptureScreen()
 {
 	HDC screenDC = GetDC(NULL);
 	BitBlt(m_screenMemoryDC, 0, 0, m_screenWidth, m_screenHeight, screenDC, 0, 0, SRCCOPY);
@@ -497,7 +504,7 @@ void Capture()
 				buffer[index++] = color >> 8;
 			buffer[index++] = color;
 			if (index >= sizeof(buffer) - 8) {
-				hr = SendData(buffer, index);
+				hr = SendData(m_clientScreenSocket, buffer, index);
 				m_sentBytes += index;
 				index = 0;
 				if (hr != S_OK)
@@ -510,7 +517,7 @@ void Capture()
 
 	if (updatedPixels) {
 		if (index) {
-			SendData(buffer, index);
+			SendData(m_clientScreenSocket, buffer, index);
 			m_sentBytes += index;
 		}
 		m_updatedPixels += updatedPixels;
@@ -522,13 +529,13 @@ void Capture()
 
 }
 
-void Send(LPVOID p)
+void SendScreen(LPVOID p)
 {
 	HWND hWnd = (HWND)p;
 	while (m_running) {
-		if (IsDisconnected())
+		if (IsSocketDisconnected(m_clientScreenSocket))
 		{
-			m_startTime = m_updatedPixels = m_sentFrames = m_sentBytes = m_receivedBytes = 0;
+			m_startTime = m_updatedPixels = m_sentBytes = m_receivedBytes = m_sentFrames = 0;
 			if (m_szText[0]) {
 				m_szText[0] = 0;
 				InvalidateRect(hWnd, NULL, TRUE);
@@ -543,17 +550,17 @@ void Send(LPVOID p)
 		{
 			static LONGLONG s_tick = GetTickCount64();
 			static LONGLONG s_sentBytes = 0;
-			static LONGLONG s_sentFrames = 0;
+			static DWORD s_sentFrames = 0;
 			LONGLONG tick = GetTickCount64();
-			Capture();
+			CaptureScreen();
 			DWORD bps = (DWORD)(m_sentBytes - s_sentBytes);
-			DWORD fps = (DWORD)(m_sentFrames - s_sentFrames);
+			DWORD fps = m_sentFrames - s_sentFrames;
 			DWORD bpf = fps ? bps / fps : 0;
 			if (GetTickCount64() - s_tick >= 1000)
 			{
-				LONGLONG time = tick - m_startTime;
-				sprintf_s(m_szText, "pixels %llu bytes %llu/%llu frames %llu time %llu bpf %d  bps %d fps %d",
-					m_updatedPixels, m_sentBytes, m_receivedBytes, m_sentFrames, time, bpf, bps, fps);
+				DWORD time = (DWORD)((tick - m_startTime) / 1000);
+				sprintf_s(m_szText, "pixels %5.3fM sent %5.3fMB received %5.3fKB frames %d time %02d:%02d bpf %d bps %d fps %d",
+					(float)m_updatedPixels/(1000*1000), (float)m_sentBytes/(1024 *1024), (float)m_receivedBytes/1024, m_sentFrames, time/60, time%60, bpf, bps, fps);
 				s_tick = tick;
 				s_sentBytes = m_sentBytes;
 				s_sentFrames = m_sentFrames;
@@ -574,16 +581,179 @@ void Send(LPVOID p)
 	}
 }
 
-void Receive(LPVOID p)
+void ReceiveDesktopControl(LPVOID p)
 {
 	while (m_running) {
-		if (IsDisconnected())
-			Accept();
+		if (IsSocketDisconnected(m_clientScreenSocket)) {
+			if (Accept(m_clientScreenSocket, m_listenScreenSocket) == S_OK)
+				InitScreenCapture();
+		}
 		else
-			Read();
+			ReadDesktopControl();
 	}
 }
 
+#ifdef REMOTE_AUDIO
+
+#include <audioclient.h>
+#include <mmdeviceapi.h>
+
+SOCKET m_clientAudioSocket = INVALID_SOCKET;
+SOCKET m_listenAudioSocket = INVALID_SOCKET;
+
+#define REFTIMES_PER_SEC  10000000
+#define REFTIMES_PER_MILLISEC  10000
+
+
+#define EXIT_ON_ERROR(hres) if (FAILED(hres)) { goto Exit; }
+#define SAFE_RELEASE(punk)  if ((punk) != NULL) { (punk)->Release(); (punk) = NULL; }
+
+const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
+const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
+const IID IID_IAudioClient = __uuidof(IAudioClient);
+const IID IID_IAudioCaptureClient = __uuidof(IAudioCaptureClient);
+
+void SendAudio(LPVOID)
+{
+	HRESULT hr;
+	REFERENCE_TIME hnsRequestedDuration = REFTIMES_PER_SEC;
+	REFERENCE_TIME hnsActualDuration;
+	UINT32 bufferFrameCount;
+	UINT32 numFramesAvailable;
+	IMMDeviceEnumerator *pEnumerator = NULL;
+	IMMDevice *pDevice = NULL;
+	IAudioClient *pAudioClient = NULL;
+	IAudioCaptureClient *pCaptureClient = NULL;
+	UINT32 packetLength = 0;
+	BOOL bDone = FALSE;
+	BYTE *pData;
+	DWORD flags;
+
+	hr = CoCreateInstance(
+		CLSID_MMDeviceEnumerator, NULL,
+		CLSCTX_ALL, IID_IMMDeviceEnumerator,
+		(void**)&pEnumerator);
+	EXIT_ON_ERROR(hr)
+
+	hr = pEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &pDevice);
+	EXIT_ON_ERROR(hr)
+
+	hr = pDevice->Activate(IID_IAudioClient, CLSCTX_ALL,	NULL, (void**)&pAudioClient);
+	EXIT_ON_ERROR(hr)
+
+	WAVEFORMATEX* pwfx;
+	hr = pAudioClient->GetMixFormat(&pwfx);
+	WAVEFORMATEXTENSIBLE* pwfe = (WAVEFORMATEXTENSIBLE*)pwfx;
+
+	sprintf_s(Log(), "format %X, channels %d, samples %d, bits %d, block %d, abps %d, cbs %d",
+		pwfx->wFormatTag, pwfx->nChannels, pwfx->nSamplesPerSec, pwfx->wBitsPerSample, pwfx->nBlockAlign, pwfx->nAvgBytesPerSec, pwfx->cbSize);
+
+	const char * format = "";
+	if (KSDATAFORMAT_SUBTYPE_PCM == pwfe->SubFormat)
+		format = "KSDATAFORMAT_SUBTYPE_PCM";
+	else if (KSDATAFORMAT_SUBTYPE_IEEE_FLOAT == pwfe->SubFormat)
+		format = "KSDATAFORMAT_SUBTYPE_IEEE_FLOAT";
+	else if (KSDATAFORMAT_SUBTYPE_DRM == pwfe->SubFormat)
+		format = "KSDATAFORMAT_SUBTYPE_DRM";
+	else if (KSDATAFORMAT_SUBTYPE_ALAW == pwfe->SubFormat)
+		format = "KSDATAFORMAT_SUBTYPE_ALAW";
+	else if (KSDATAFORMAT_SUBTYPE_MULAW == pwfe->SubFormat)
+		format = "KSDATAFORMAT_SUBTYPE_MULAW";
+	else if (KSDATAFORMAT_SUBTYPE_ADPCM == pwfe->SubFormat)
+		format = "KSDATAFORMAT_SUBTYPE_ADPCM";
+
+	sprintf_s(Log(),"format %s, bits %d, samples %d, mask %d", format, pwfe->Samples.wValidBitsPerSample, pwfe->Samples.wSamplesPerBlock, pwfe->dwChannelMask);
+	
+	hr = pAudioClient->Initialize(AUDCLNT_SHAREMODE_SHARED,	AUDCLNT_STREAMFLAGS_LOOPBACK, hnsRequestedDuration,	0, pwfx, NULL);
+	EXIT_ON_ERROR(hr)
+
+	// Get the size of the allocated buffer.
+	hr = pAudioClient->GetBufferSize(&bufferFrameCount);
+	EXIT_ON_ERROR(hr)
+
+	hr = pAudioClient->GetService(IID_IAudioCaptureClient, (void**)&pCaptureClient);
+	EXIT_ON_ERROR(hr)
+
+	// Calculate the actual duration of the allocated buffer.
+	hnsActualDuration = ((LONGLONG)REFTIMES_PER_SEC * bufferFrameCount) / pwfx->nSamplesPerSec;
+
+	hr = pAudioClient->Start();  // Start recording.
+	EXIT_ON_ERROR(hr)
+
+	// Each loop fills about half of the shared buffer.
+	while (true)
+	{
+		// Sleep for half the buffer duration.
+		Sleep((DWORD)(hnsActualDuration / REFTIMES_PER_MILLISEC / 2));
+
+		hr = pCaptureClient->GetNextPacketSize(&packetLength);
+		EXIT_ON_ERROR(hr)
+
+		while (packetLength != 0)
+		{
+			// Get the available data in the shared buffer.
+			hr = pCaptureClient->GetBuffer(
+				&pData,
+				&numFramesAvailable,
+				&flags, NULL, NULL);
+
+			EXIT_ON_ERROR(hr)
+
+			if (flags)
+			{
+				pData = NULL;  // Tell CopyData to write silence.
+			}
+			else
+			{
+				char buffer[8000];
+				int shrink = pwfx->nSamplesPerSec / 8000;
+				numFramesAvailable /= shrink;
+				shrink *= 2;
+				float* frame = (float*)pData;
+				for (UINT i = 0; i < numFramesAvailable; i++) {
+					buffer[i] = (char)(*frame);
+					frame += shrink;
+				}
+				// Copy the available capture data to the audio sink.
+				hr = SendData(m_clientAudioSocket, buffer, numFramesAvailable);
+				EXIT_ON_ERROR(hr)
+			}
+
+			hr = pCaptureClient->ReleaseBuffer(numFramesAvailable);
+			EXIT_ON_ERROR(hr)
+
+			hr = pCaptureClient->GetNextPacketSize(&packetLength);
+			EXIT_ON_ERROR(hr)
+		}
+	}
+
+	hr = pAudioClient->Stop();  // Stop recording.
+	EXIT_ON_ERROR(hr)
+
+Exit:
+	CoTaskMemFree(pwfx);
+	SAFE_RELEASE(pEnumerator)
+	SAFE_RELEASE(pDevice)
+	SAFE_RELEASE(pAudioClient)
+	SAFE_RELEASE(pCaptureClient)
+
+}
+
+void ReceiveAudio(LPVOID p)
+{
+	while (m_running) {
+		if (IsSocketDisconnected(m_clientAudioSocket)) {
+			if (Accept(m_clientAudioSocket, m_listenAudioSocket) == S_OK)
+				_beginthread(SendAudio, 0, p);
+		}
+		else {
+			char buffer[512];
+			ReadBytes(m_clientAudioSocket, buffer, sizeof(buffer));
+			//ReadAudio();
+		}			
+	}
+}
+#endif
 //
 //   FUNCTION: InitInstance(HINSTANCE, int)
 //
@@ -611,12 +781,19 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
 	InitLog();
 
-	if (InitSocket(hWnd) != S_OK)
-		return TRUE;
+	if (InitSocketSystem() != S_OK)
+		return FALSE;
 
-	_beginthread(Receive, 0, hWnd);
-	_beginthread(Send, 0, hWnd);
+	if (InitSocket(m_listenScreenSocket, 18080) != S_OK)
+		return FALSE;
 
+	_beginthread(ReceiveDesktopControl, 0, hWnd);
+	_beginthread(SendScreen, 0, hWnd);
+
+#ifdef REMOTE_AUDIO
+	if (InitSocket(m_listenAudioSocket, 18081) == S_OK && CoInitializeEx(0, COINIT_MULTITHREADED) == S_OK)		
+		_beginthread(ReceiveAudio, 0, 0);
+#endif
 	return TRUE;
 }
 
@@ -679,14 +856,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 #endif
 	case WM_DESTROY:
 		m_running = false;
-		if (m_clientSocket != INVALID_SOCKET) {
-			shutdown(m_clientSocket, SD_SEND);
-			m_clientSocket = INVALID_SOCKET;
+		if (m_clientScreenSocket != INVALID_SOCKET) {
+			shutdown(m_clientScreenSocket, SD_SEND);
+			m_clientScreenSocket = INVALID_SOCKET;
 		}
-		if (m_listenSocket != INVALID_SOCKET) {
-			shutdown(m_listenSocket, SD_RECEIVE);
-			m_listenSocket = INVALID_SOCKET;
+		if (m_listenScreenSocket != INVALID_SOCKET) {
+			shutdown(m_listenScreenSocket, SD_RECEIVE);
+			m_listenScreenSocket = INVALID_SOCKET;
 		}
+#ifdef REMOTE_AUDIO
+		if (m_clientAudioSocket != INVALID_SOCKET) {
+			shutdown(m_clientAudioSocket, SD_SEND);
+			m_clientAudioSocket = INVALID_SOCKET;
+		}
+		if (m_listenAudioSocket != INVALID_SOCKET) {
+			shutdown(m_listenAudioSocket, SD_RECEIVE);
+			m_listenAudioSocket = INVALID_SOCKET;
+		}
+#endif
 		Sleep(100);
 		GlobalFree(m_screenDIB);
 		GlobalFree(m_screenBuffer);
